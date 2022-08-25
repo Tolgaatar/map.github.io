c023928a76748322b2b43f58ccf8148e944715e6
Tolgaatar <99403581+Tolgaatar@users.noreply.github.com>
2022-08-25 15:14:56 +0300

    tolga2

--- location.js
+++ location.js
@@ -1,16 +1,20 @@
 
-let mapOptions = {
-    center:[40.989, 29.027],
-    zoom:15,
-    tap: false
-}
+var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {
+    attribution: osmAttrib,
+    detectRetina: true
+});
 
 
 
-let map = new L.map('map' , mapOptions);
-
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-map.addLayer(layer);
+var map = new L.Map('map', {
+    tap: false, // ref https://github.com/Leaflet/Leaflet/issues/7255
+    layers : osm,
+    center: [51.505, -0.09],
+    zoom: 10,
+    zoomControl: true
+});
 
 
 let locations = [
@@ -64,9 +68,8 @@ locations.forEach(element => {
 });
 
 
-
-map.addControl(L.control.locate({
-    locateOptions: {
-            enableHighAccuracy: true
-}}));
-
+lc = L.control.locate({
+    strings: {
+        title: "Show me where I am, yo!"
+    }
+}).addTo(map);
